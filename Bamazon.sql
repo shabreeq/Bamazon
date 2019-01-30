7f644ed7ce66fe181a91baa337d5855a8d689934
ShaBree Quinones <shabreeq@gmail.com>
2019-01-30 18:09:27 -0500

    initial commit

---
+++ Bamazon.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE Bamazon;
+
+USE Bamazon;
+
+CREATE TABLE Products(
+
+    Item_id INT NOT NULL,
+  Product_Name VARCHAR(100) NOT NULL,
+  Department_Name VARCHAR(100) NOT NULL,
+  Price DECIMAL (10,4) NOT NULL,
+  Stock_Quantity INT NOT NULL,
+  PRIMARY KEY (Item_id)
+  
+);
+
+
+
+
---
+++ DB_Bamazon.js
@@ -0,0 +1,43 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Sq120899680",
+  database: "top_songsDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  runSearch();
+});
+
+function runSearch() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "default",
+      message: "What is the ID of the product you would like to buy?",
+    
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+      case "Find product":
+        // return product information from table
+        
+        break;
+
+        connection.end();
+        break;
+      }
+    });
+}
+
