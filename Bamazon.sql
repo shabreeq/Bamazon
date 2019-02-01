30ab60a1c3b2de0395f695f546634da9c34e0779
ShaBree Quinones <shabreeq@gmail.com>
2019-01-31 22:22:37 -0500

    saving work

--- Bamazon.sql
+++ Bamazon.sql
@@ -4,7 +4,7 @@ USE Bamazon;
 
 CREATE TABLE Products(
 
-    Item_id INT NOT NULL,
+  Item_id INT NOT NULL,
   Product_Name VARCHAR(100) NOT NULL,
   Department_Name VARCHAR(100) NOT NULL,
   Price DECIMAL (10,4) NOT NULL,
--- DB_Bamazon.js
+++ DB_Bamazon.js
@@ -1,5 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var cTable = require("console.table");
+require('dotenv').config()
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -11,51 +13,86 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "Sq120899680",
-  database: "top_songsDB"
+  password: process.env.SQL_Password,
+  database: "Bamazon"
 });
 
 connection.connect(function (err) {
   if (err) throw err;
-  runSearch();
+  displayProducts();
 });
 
 function runSearch() {
-  inquirer
-    .prompt({
-      name: "action",
-      type: "default",
-      message: "What is the ID of the product you would like to buy?",
-
-    })
-    .then(function (answer) {
-      switch (answer.action) {
-        case "Find product":
-          // return product information from table
-
-          break;
-
-          connection.end();
-          break;
-      }
-    });
+  //inquirer.prompt([]).then(function(answers){});
 
   inquirer
-    .prompt({
-      name: "action",
-      type: "default",
-      message: "How many units would you like to purchase?",
+    .prompt([{
+      name: "pID",
+      message: "What is the ID of the product you would like to buy?"
 
-    })
+    },{
+      name: "units",
+      message: "How many units would you like to purchase?"
+
+    }])
     .then(function (answer) {
-      switch (answer.action) {
-        case "Find product":
-          // return product information from table
+     
+      console.log(answer.pID , answer.units);
 
-          break;
-
-          connection.end();
-          break;
-      }
+      makePurchase(answer.pID, answer.units);
+     
+      
     });
+
 }
+
+// Display all Products
+const displayProducts = () => {
+  let qsel = ("SELECT * FROM Bamazon.products");
+  
+  const query = connection.query(qsel, function(err, results){
+    if (err) throw err;
+    // do stuff here
+    console.table(results);
+    runSearch();    
+  } );
+  
+
+};
+
+// To UPdate Product
+const updateProduct = (id, quantity) => {
+  let qsel = ("UPDATE PRODUCTS SET ? WHERE ?");
+  
+  const query = connection.query(qsel, [{stock_quantity:quantity},{item_id:id}], function(err, results){
+    if (err) throw err;
+    // do stuff here
+    console.log(results.affectedRows);
+    displayProducts();    
+  } );
+  
+
+};
+
+// Make Prurchase
+
+// function makePurchase(id, units){};
+const makePurchase = (id, units) => {
+  let qsel = ("SELECT * FROM Bamazon.products where ?");
+  console.log("in here");
+  const query = connection.query(qsel, [{item_id:id}], function(err, results){
+    if (err) throw err;
+    // do stuff here
+
+    let eStockQ = results[0].Stock_Quantity;
+    if(eStockQ < units){
+      console.log("Insuffgldjflkgj");
+    }else{
+      let quan = eStockQ - units;
+      updateProduct(id,quan);
+    }
+
+  } );
+  
+
+};
